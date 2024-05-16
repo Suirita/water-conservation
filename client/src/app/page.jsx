d87c797c2be66aff4cbee6a2a7db7605d6b276b8
Sarrouj <zaydssh500@gmail.com>
2024-05-16 16:40:34 +0100

    Feat  : Routing Folders

---
+++ client/src/app/page.jsx
@@ -0,0 +1,23 @@
+
+export default function Home() {
+  return (
+   <main>
+    <div></div>
+    <div>
+      <div>
+        <h2>Login</h2>
+        <p>Enter Your Information to Login</p>
+      </div>
+      <div>
+        <div>
+          <p>Email</p>
+          <input type="text" />
+        </div>
+        <div>
+          
+        </div>
+      </div>
+    </div>
+   </main>
+  );
+}
