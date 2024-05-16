d32491182449b38bc354e1c4bbc52c4954b4bf83
BRIHI <brihihossam@gmail.com>
2024-05-16 18:13:52 +0100

    WIP

--- client/src/app/page.jsx
+++ client/src/app/page.jsx
@@ -1,23 +1,9 @@
+import Consumption from "@/Components/Consumption";
 
 export default function Home() {
   return (
-   <main>
-    <div></div>
-    <div>
-      <div>
-        <h2>Login</h2>
-        <p>Enter Your Information to Login</p>
-      </div>
-      <div>
-        <div>
-          <p>Email</p>
-          <input type="text" />
-        </div>
-        <div>
-          
-        </div>
-      </div>
-    </div>
-   </main>
+  
+   <Consumption/>
   );
 }
+
