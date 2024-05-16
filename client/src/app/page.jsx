10e84db645d2f2060b96a9ed95de39bf0a31b18d
tribak77 <atribak34@gmail.com>
2024-05-16 17:40:48 +0100

    add something

--- client/src/app/page.jsx
+++ client/src/app/page.jsx
@@ -1,23 +1,7 @@
+import Statistics from "@/Components/Statistics";
 
 export default function Home() {
   return (
-   <main>
-    <div></div>
-    <div>
-      <div>
-        <h2>Login</h2>
-        <p>Enter Your Information to Login</p>
-      </div>
-      <div>
-        <div>
-          <p>Email</p>
-          <input type="text" />
-        </div>
-        <div>
-          
-        </div>
-      </div>
-    </div>
-   </main>
+   <Statistics/>
   );
 }
