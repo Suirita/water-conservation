d8ed22671407d76d336e8e5798bc769eff538aef
Sarrouj <zaydssh500@gmail.com>
2024-05-16 17:48:12 +0100

    Work In progress

--- client/src/app/page.jsx
+++ client/src/app/page.jsx
@@ -1,21 +1,28 @@
 
 export default function Home() {
   return (
-   <main>
-    <div></div>
-    <div>
+   <main className="px-10 py-10 flex">
+    <div className="w-2/4 bg-blue-200 h-screen p-10 rounded-lg">
+      <h1 className="text-white font-bold text">AmanWater</h1>
+    </div>
+    <div className="px-10">
       <div>
         <h2>Login</h2>
         <p>Enter Your Information to Login</p>
       </div>
       <div>
         <div>
-          <p>Email</p>
-          <input type="text" />
+          <label htmlFor="email"></label>
+          <input type="email" placeholder="m@gmail.com"/>
         </div>
         <div>
-          
+          <div className="flex">
+            <label htmlFor="password">Password</label>
+            <p>Forgot your Email ?</p>
+          </div>
+          <input type="password" placeholder="Password (8 or more characters)"/>
         </div>
+        <input type="button" value={"Sign In"}/>
       </div>
     </div>
    </main>
